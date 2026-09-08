57f05e96e9ab72f0b992ef8ea959e0432841bf5a
AGUSTIN\DESKTOP <agustinsanchezdev@gmail.com>
2026-09-08 13:49:54 -0300

    nueva configuracion app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 $envFile = $_SERVER['APP_ENV_FILE'] ?? '.env';
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->loadEnvironmentFrom($envFile)
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -35,4 +34,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
+
+$app->loadEnvironmentFrom($envFile);
+
+return $app;
